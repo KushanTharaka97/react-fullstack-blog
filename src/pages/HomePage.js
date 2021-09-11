f945e7a35b38d2ebb5a2b4281ecb17b5e4e19792
Kushan Tharaka <tharakakushan@gmail.com>
2021-09-11 06:19:30 +0530

    Modified.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from  'react';
+
+const HomePage = () => (
+	<React.Fragment>
+	<h1>Welcome to the blog! </h1>
+	<p>
+		Elit magna consequat derby day chantilly anim bourne hall. Salts downs horses. Stables anim oaks epsom salts. 
+	</p>
+
+	<p>
+	Nisi culpa non epsom mollit ea. In minim fugiat eiusmod officia chantilly minim derby. Ea enim id epsom salts laboris. Elit exercitation nulla.
+	Exercitation racing ewell. Sunt ullamco exercitation derby day epsom downs horton sed chantilly. Epsom voluptate id cillum bourne hall exercitation excepteur cillum. 
+	</p>
+
+<p>
+Elit magna consequat derby day chantilly anim bourne hall. Salts downs horses. Stables anim oaks epsom salts.
+Nisi culpa non epsom mollit ea. In minim fugiat eiusmod officia chantilly minim derby. Ea enim id epsom salts laboris. Elit exercitation nulla.
+Exercitation racing ewell. Sunt ullamco exercitation derby day epsom downs horton sed chantilly. Epsom voluptate id cillum bourne hall exercitation excepteur cillum. 
+</p>
+	</React.Fragment>
+	);
+
+export default HomePage;
+
